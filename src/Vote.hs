19321430d08c9478673f32d0a208f70c5f24fbce
Clemens Lieb <vogel612@gmx.de>
2016-09-01 14:05:03 +0200

    Added function to parse a string of vote-data into a Vote.

--- src/Vote.hs
+++ src/Vote.hs
@@ -1,6 +1,10 @@
 data Vote = Vote {
-   first :: Integer,
-   second :: Integer,
-   third :: Integer
+   first :: Int,
+   second :: Int,
+   third :: Int
 } deriving (Eq, Show)
 
+fromString :: String -> Maybe Vote 
+fromString line = case words line of
+    f:s:t:_ ->  Just $ Vote (read f) (read s) (read t)
+    _ -> Nothing
