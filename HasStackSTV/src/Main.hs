ea8bbd0d5588a270a0ec0ca953dd1fa2d39838ba
Clemens Lieb <vogel612@gmx.de>
2017-01-13 15:04:35 +0100

    add main implementation

--- HasStackSTV/src/Main.hs
+++ HasStackSTV/src/Main.hs
@@ -2,6 +2,21 @@ module Main (
     main
 ) where
 
+import System.IO(readFile, getContents)
+import Vote(fromString)
+import Election(Election(..), runElection, ElectionResults(..), combineVotes)
+import Candidate(Candidate(..))
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+    contents <- readFile "election-data.blt"
+    let ln = lines contents
+    let e = head ln
+    let votes = combineVotes (map fromString $ takeWhile (\f -> f /= "0" && f /= "0\r") $ tail ln) []
+    let candidates = map (\(i, n) -> Candidate n i) $ zip [1..] $ tail $ dropWhile (\f -> f /= "0" && f /= "0\r") $ tail ln
+    let election = Election (read $ (words e)!!1) candidates votes
+    putStrLn $ "Running Election\n"++(show election)
+    let results = runElection election
+    putStrLn "finished"
+    putStrLn $ show results
 
